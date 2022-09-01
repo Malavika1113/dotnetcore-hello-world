05fae55f179dcb41f00fe1d059bbffd853487a5f
Malavika1113 <74175454+Malavika1113@users.noreply.github.com>
2022-09-01 15:56:36 +0530

    Update validate.sh

--- .github/scripts/validate.sh
+++ .github/scripts/validate.sh
@@ -14,7 +14,7 @@ for env in "${envArray[@]}"; do
 	if [[ "${env}" = "${releaseEnv}" && "${releaseVersion}" != "1.0-SNAPSHOT" ]];then
 		echo "Success: For given release version: ${releaseVersion}, environment: ${releaseEnv} is valid"
 		exit 0
-	fi
-	echo "Error: For given release version: ${releaseVersion}, environment: ${releaseEnv} is invalid. Valid release version format xx.xx.xx.xx Eg: 22.03.0.1 "
- 	exit 1
+	fi	
 done
+echo "Error: For given release version: ${releaseVersion}, environment: ${releaseEnv} is invalid. Valid release version format xx.xx.xx.xx Eg: 22.03.0.1 "
+exit 1
