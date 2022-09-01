0d46b4ffceb8f54e837c8e3c87db177b25e70d87
Malavika1113 <74175454+Malavika1113@users.noreply.github.com>
2022-09-01 10:54:50 +0530

    Update validate.sh

--- .github/scripts/validate.sh
+++ .github/scripts/validate.sh
@@ -7,12 +7,12 @@ declare -a envArray=("test" "dev" "prod")
 
 for env in "${envArray[@]}"; do
 	if [[ "${releaseVersion}" != "1.0-SNAPSHOT" ]];then
-		echo "Success: For given environment: ${releaseEnv} is valid"
+		echo "Success: For given release version: ${releaseVersion}, environment: ${releaseEnv} is valid"
 		exit 0
 	fi
 done
 
-echo "Error: For given environment: ${releaseEnv} is invalid. Valid environments choices: "
+echo "Error: For given release version: ${releaseVersion}, environment: ${releaseEnv} is invalid. Valid environments choices: "
 for env in "${envArray[@]}"; do
 	echo "${env}"
 done
